cb56972c5edf1061168e24d13f5187027f136bc8
smhrd <smhrd@smart>
2024-06-12 11:37:18 +0900

    4141

---
+++ FoodDeuk-main/Maven_FDD/src/main/java/com/smhrd/model/BookDAO.java
@@ -0,0 +1,31 @@
+package com.smhrd.model;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.smhrd.database.SqlSessionManager;
+
+
+public class BookDAO {
+	SqlSessionFactory sqlSessionFactory= SqlSessionManager.getSqlSession();
+	public int join(Member member) {
+		SqlSession session=sqlSessionFactory.openSession(true);
+		
+		//session.insert(경로, 넘겨줄 파라미터);
+		//실행 시 정수 데이터 return (0,1)            이거는 namespace
+		int cnt = session.insert("com.smhrd.database.MemberMapper.join", member);
+		session.close();
+		return cnt;
+	}
+
+	public Member login(Member member) {
+		SqlSessionFactory sqlSessionFactory= SqlSessionManager.getSqlSession();
+		SqlSession session=sqlSessionFactory.openSession(true);
+		
+		Member login_member= session.selectOne("com.smhrd.database.MemberMapper.login", member);
+		session.close();
+		return login_member;
+		
+		
+	}
+
+}
